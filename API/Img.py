41a680c8e6f884775f7f543becc4deee4770f831
halkliff <werberth.lins@gmail.com>
2016-12-05 11:05:29 -0200

    Complete change in the API(Not working)
    An error is always raised.

--- API/Img.py
+++ API/Img.py
@@ -3,63 +3,147 @@
 import requests
 import random
 
+
+SITE_LIST = {
+    'konachan': {
+        'url': "http://konachan.com",
+        'hashed_string': "So-I-Heard-You-Like-Mupkids-?--{0}--"},
+
+    'yandere': {
+        'url': "https://yande.re",
+        'hashed_string': "choujin-steiner--{0}--"}
+    }
+
+API_NAME={
+        'posts_list':
+            '/post.json?',
+    'tags_list':
+        '/tag.json'
+
+    }
+
+PARAMS = {
+    'limit':
+        '&limit=1',
+    'page':
+        '&page=',
+    'safe':
+        '&tags=rating%3Asafe',
+    'login':
+        'login={username}',
+    'password_hash':
+        '&password_hash={password_hash}',
+    'for_anime':
+        '&tags=rating%3Asafe+-pantyshot+-panties+&ms=1',
+    'for_ecchi':
+        '&tags=rating%3Aquestionable+&ms=1',
+    'for_loli':
+        '&tags=loli+',
+    'for_yuri':
+        '&tags=yuri+',
+    'for_yaoi':
+        '&tags=yaoi+'
+    }
+
 class Requests:
-    def __init__(self, id, created_at, uploader_id, uploader_name, score, source,
-                 md5, rating, image_width, image_height, tag_string, tag_string_general,
-                 tag_count_artist, pixiv_id, file_url, large_file_url, ):
+    def __init__(self, username="", password_hash="", site_name="", api_name="", param="", site_url=None):
+        print("API initialized")
+
+        self.site_name = site_name.lower()
+        self.api_name = api_name.lower()
+        self.param = param
+        self.site_url = site_url
+        self.username = username
+        self.password_hash = password_hash
+
+        if site_name is not "" or api_name is not "" or param is not "":
+            if site_name in list(SITE_LIST):
+                self.site_url = SITE_LIST[site_name]['url']
+            else:
+                raise Exception("Check again the site_name")
+            if api_name in list(API_NAME):
+                self.api_name = API_NAME[api_name]
+            else:
+                raise Exception("Check again the api_name")
+            if param in list(PARAMS):
+                self.param = PARAMS[param]
+            else:
+                raise Exception("Check again the api_name")
+        else:
+            raise Exception("Woops, don't forget to fulfill site_name, api_name and the param.")
+
+        if username and password_hash is not "":
+            if username in api_login_info.username:
+                self.username = PARAMS['login'].format(username=username)
+            else:
+                raise Exception("Username not found")
+            if password_hash in api_login_info.password_hash:
+                self.password_hash = PARAMS['password_hash'].format(password_hash=password_hash)
+            else:
+                raise Exception("Wrong Password Hash")
+        else:
+            self.password_hash = '&password_hash=none'
+            self.username = 'login=none'
+
+
+class Posts:
+    def __init__(self, id, tags, creator_id, author, source, score, md5, file_url, sample_url, width, height, **kwargs):
         self.id = id
-        self.created_at = created_at
-        self.uploader_id = uploader_id
-        self.uploader_name = uploader_name
-        self.score = score
+        self.tags = tags
+        self.creator_id = creator_id
+        self.author = author
         self.source = source
+        self.score = score
         self.md5 = md5
-        self.rating = rating
-        self.image_width = image_width
-        self.image_height = image_height
-        self.tag_string = tag_string
-        self.tag_string_general = tag_string_general
-        self.tag_count_artist = tag_count_artist
-        self.pixiv_id = pixiv_id
         self.file_url = file_url
-        self.large_file_url = large_file_url
+        self.sample_url = sample_url
+        self.width = width
+        self.height = height
+        self.other_width = kwargs.get("jpeg_width")
+        self.other_height = kwargs.get("jpeg_height")
+        self.other_file_url = kwargs.get("jpeg_url")
+
+        #for the sake of consistency, if it doesn't exists, it uses the other
+        if width or height or file_url is None:
+            self.width = self.other_width
+            self.height = self.other_height
+            self.file_url = self.other_file_url
+
+        if sample_url is None:
+            raise Exception("Error: Image could not be found in the link", sample_url)
+        if source is None:
+            self.source = "No Source could be found"
+
+generic_url = '{site_url}{api_name}{login}{password}{param}{page}{limit}'
+
+def anime():
+    l = Requests(site_name='yandere',
+                 api_name='posts_list', param='for_anime')
+    random_page = random.sample(range(1, 183363), 1)
+    random_page_number = random_page[0]
+
+    url = generic_url.format(site_url=l.site_url, api_name=l.api_name, login=l.username,
+                                           password=l.password_hash, param=l.param, page='&page='+str(int(random_page_number)),
+                                           limit=PARAMS['limit'])
+    print(url)
+    req = requests.get(url)
 
 
+    ret = req.json()
+    id = ret["id"]
+    tags = ret["tags"]
+    creator_id = ret["creator_id"]
+    author = ret["author"]
+    source = ret["source"]
+    score = ret["score"]
+    md5 = ret["md5"]
+    file_url = ret["file_url"]
+    sample_url = ret["sample_url"]
+    width = ret["width"]
+    height = ret["width"]
 
-Danbooru = 'http://danbooru.donmai.us'
-Dreq = requests.get(Danbooru + '/posts.json?&limit=2000000')
+    data = Posts(id, tags, creator_id, author, source, score, md5, file_url, sample_url, width, height)
+    print(data)
+    all = data.tags, data.creator_id, data.author, data.source, data.score, data.md5, data.file_url, data.sample_url, data.width, data.height
 
-loaded_data = Dreq.json()
-
-last_num = len(loaded_data)
-random_list = random.sample(range(1, last_num), 1)
-randomly_chosen_number = random_list[ 0 ]
-
-r = requests.get(Danbooru + '/posts/' + str(loaded_data[ int(randomly_chosen_number) ][ "id" ]) + '.json')
-data = Requests(r.json()["id"], r.json()["created_at"], r.json()["uploader_id"], r.json()["uploader_name"], r.json()["score"],
-                    r.json()["source"], r.json()["md5"], r.json()["rating"], r.json()["image_width"], r.json()["image_height"],
-                    r.json()["tag_string"], r.json()["tag_string_general"], r.json()["tag_count_artist"], r.json()["pixiv_id"],
-                    r.json()["file_url"], r.json()["large_file_url"])
-
-#==================================================================================================================
-#Data Working for import
-id = data.id
-created_at = data.created_at
-uploader_id = data.uploader_id
-uploader_name = data.uploader_name
-score = data.score
-source = data.source
-md5 = data.md5
-rating = data.rating
-image_width = data.image_width
-image_height = data.image_height
-tag_string = data.tag_string
-tag_string_general = data.tag_string_general
-tag_count_artist = data.tag_count_artist
-pixiv_id = data.pixiv_id
-file_url = data.file_url
-large_file_url = data.large_file_url
-
-
-if __name__=="__main__":
-    print("Imported!")
+    return all
