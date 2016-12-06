dc2158c1817f2caa2c835686fa5f9527b441f8b5
halkliff <werberth.lins@gmail.com>
2016-12-06 17:38:11 -0200

    Added __init__.py and Config.py
    The __init__.py serves to give info about the creator, and Config.py is used to set the main things of the bot.

---
+++ __init__.py
@@ -0,0 +1,19 @@
+# Main resources for the bot to work
+
+"""
+Emα Project, 2016
+--------------------
+
+Emα is a bot, which works under Telegram Bot API, with support of PyTelegramBotAPI, by etnoir.
+
+See the Telegram Bot API: https://core.telegram.org/bots/api
+
+the PyTelegramBotAPI, by etnoir: https://github.com/eternnoir/pyTelegramBotAPI/blob/master/README.md
+"""
+
+__author__ = "Halk Liff <HalkLiff@protonmail.ch>"
+__version__ = "1.3.1 BETA"
+__url__ = "https://github.com/halkliff/EmaProject"
+__contacts__ = """Telegram: https://telegram.me/MrHalk
+                  Email: HalkLiff@protonmail.ch"""
+
