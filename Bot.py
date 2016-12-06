aa64188cc71498a7160fa440f309b75eef8cb6a9
halkliff <werberth.lins@gmail.com>
2016-12-06 17:41:37 -0200

    Updated Bot.py - Now everything works!
    Now all commands are working as expected! Also updated to work with the changes in the Language and API

--- Bot.py
+++ Bot.py
@@ -1,4 +1,5 @@
-# _||_ Code Test number 10 in utf-8 _||_
+# -*- utf-8 -*-
+# _||_ Code Test number 10 _||_
 # Exclusive use
 
 import telebot
@@ -6,11 +7,13 @@ from telebot import types
 import logging
 import time
 import sys
+# import re
 from API import Img
 from lang import EN as en
+import Config
 
 # Bot Token taken by @BotFather
-TOKEN = '327068957:AAGvW-4w53Qo-ilN4dnoXy2-mlQ1UFOUhPg'
+TOKEN = Config.TOKEN
 
 
 # Listener to see when new messages arrives
@@ -37,12 +40,12 @@ def send_welcome(m):
     cid = m.chat.id
 
     if text == "/start tags":
-        bot.send_message(cid, en.commandText['/start tags'], parse_mode='markdown')
+        bot.send_message(cid, en.commandText['/start tags'].format(bot_id=Config.BOT_ID), parse_mode='markdown')
     elif text == "/start source_id":
         bot.send_message(cid, en.commandText['/start source_id'], parse_mode='markdown')
     else:
         try:
-            bot.reply_to(m, en.commandText['start'].format(name = m.from_user.first_name),
+            bot.reply_to(m, en.commandText['start'].format(name = m.from_user.first_name, bot_name=Config.BOT_NAME),
                      parse_mode='markdown',)
         except Exception:
             print(Exception)
@@ -59,7 +62,7 @@ def send_help(m):
 @bot.message_handler(commands=['commands'])
 def send_commands(m):
     try:
-        bot.reply_to(m, en.commandText['commands'], parse_mode='markdown')
+        bot.reply_to(m, en.commandText['commands'].format(bot_id=Config.BOT_ID), parse_mode='markdown')
     except Exception:
         print(Exception)
         pass
@@ -67,7 +70,7 @@ def send_commands(m):
 @bot.message_handler(commands=['inline_help'])
 def send_inline_help(m):
     try:
-        bot.reply_to(m, en.commandText['inline_help'], parse_mode='markdown')
+        bot.reply_to(m, en.commandText['inline_help'].format(bot_id=Config.BOT_ID), parse_mode='markdown')
     except Exception:
         print(Exception)
         pass
@@ -76,42 +79,152 @@ def send_inline_help(m):
 def send_anime(m):
     cid = m.chat.id
     load = Img.anime()
-    picture = load[14]
-    width = load[8]
-    height = load[9]
-    tag = load[11]
+    picture = load[8]
+    big_picture = load[7]
+    if big_picture is None:
+        big_picture = load[8]
+    width = load[9]
+    height = load[10]
+    tag = load[1]
     uploader = load[3]
-    if load[15] is None:
-        large_file_url = load[14]
-    else:
-        large_file_url = load[15]
 
 
     keyboard = telebot.types.InlineKeyboardMarkup()
-    button = telebot.types.InlineKeyboardButton(text="Original {H} x {W}".format(H=height, W=width),
-                                                url=large_file_url)
-    button2 = telebot.types.InlineKeyboardButton(text="More", callback_data='generate')
+    button = telebot.types.InlineKeyboardButton(text="💾 Download", url=big_picture)
+    button2 = telebot.types.InlineKeyboardButton(text="More", callback_data='anime')
+
     try:
         keyboard.add(button)
         keyboard.add(button2)
 
         bot.send_chat_action(cid, 'upload_photo')
-        bot.send_photo(cid, picture,caption='Uploader:{uploader}\nTags: {tag}'.format(uploader=uploader,tag=tag[0, 1, 2],),
-                       reply_markup=keyboard)
+        bot.send_photo(cid, picture,caption='🖥Resolution: {W} x {H}\n\n👤Uploader: {uploader}\n\n#⃣Tags: {tag}'.format(H=height, W=width,
+                                                                                                          uploader=uploader,tag=tag,),
+                       reply_markup=keyboard)  # reply_markup=keyboard
     except Exception:
         bot.send_message(cid, "Wasn't able to proceed your request.")
-        print("An error Ocurred:", Exception)
+        print("An error Ocurred in /anime:", Exception)
         pass
 
 @bot.message_handler(commands=['ecchi'])
+def send_ecchi(m):
+    cid = m.chat.id
+    load = Img.ecchi()
+    picture = load[8]
+    big_picture = load[7]
+    if big_picture is None:
+        big_picture = load[8]
+    width = load[9]
+    height = load[10]
+    tag = load[1]
+    uploader = load[3]
+
+
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    button = telebot.types.InlineKeyboardButton(text="💾 Download", url=big_picture)
+    button2 = telebot.types.InlineKeyboardButton(text="More", callback_data='ecchi')
+    try:
+        keyboard.add(button)
+        keyboard.add(button2)
+
+        bot.send_chat_action(cid, 'upload_photo')
+        bot.send_photo(cid, picture,caption='🖥Resolution: {W} x {H}\n\n👤Uploader: {uploader}\n\n#⃣Tags: {tag}'.format(H=height, W=width,
+                                                                                                          uploader=uploader,tag=tag,),
+                       reply_markup=keyboard)  # reply_markup=keyboard
+    except Exception:
+        bot.send_message(cid, "Wasn't able to proceed your request.")
+        print("An error Ocurred in /ecchi:", Exception)
+        pass
 
 @bot.message_handler(commands=['loli'])
+def send_loli(m):
+    cid = m.chat.id
+    load = Img.loli()
+    picture = load[8]
+    big_picture = load[7]
+    if big_picture is None:
+        big_picture = load[8]
+    width = load[9]
+    height = load[10]
+    tag = load[1]
+    uploader = load[3]
+
+
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    button = telebot.types.InlineKeyboardButton(text="💾 Download", url=big_picture)
+    button2 = telebot.types.InlineKeyboardButton(text="More", callback_data='loli')
+    try:
+        keyboard.add(button)
+        keyboard.add(button2)
+
+        bot.send_chat_action(cid, 'upload_photo')
+        bot.send_photo(cid, picture,caption='🖥Resolution: {W} x {H}\n\n👤Uploader: {uploader}\n\n#⃣Tags: {tag}'.format(H=height, W=width,
+                                                                                                          uploader=uploader,tag=tag,),
+                       reply_markup=keyboard)  # reply_markup=keyboard
+    except Exception:
+        bot.send_message(cid, "Wasn't able to proceed your request.")
+        print("An error Ocurred in /loli:", Exception)
+        pass
 
 @bot.message_handler(commands=['hentai'])
+def send_hentai(m):
+    cid = m.chat.id
+    load = Img.hentai()
+    picture = load[8]
+    big_picture = load[7]
+    if big_picture is None:
+        big_picture = load[8]
+    width = load[9]
+    height = load[10]
+    tag = load[1]
+    uploader = load[3]
 
-@bot.message_handler(commands=['yaoi'])
+
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    button = telebot.types.InlineKeyboardButton(text="💾 Download", url=big_picture)
+    button2 = telebot.types.InlineKeyboardButton(text="More", callback_data='hentai')
+    try:
+        keyboard.add(button)
+        keyboard.add(button2)
+
+        bot.send_chat_action(cid, 'upload_photo')
+        bot.send_photo(cid, picture,caption='🖥Resolution: {W} x {H}\n\n👤Uploader: {uploader}\n\n#⃣Tags: {tag}'.format(H=height, W=width,
+                                                                                                          uploader=uploader,tag=tag,),
+                       reply_markup=keyboard)  # reply_markup=keyboard
+    except Exception:
+        bot.send_message(cid, "Wasn't able to proceed your request.")
+        print("An error Ocurred in /hentai:", Exception)
+        pass
 
 @bot.message_handler(commands=['yuri'])
+def send_yuri(m):
+    cid = m.chat.id
+    load = Img.yuri()
+    picture = load[8]
+    big_picture = load[7]
+    if big_picture is None:
+        big_picture = load[8]
+    width = load[9]
+    height = load[10]
+    tag = load[1]
+    uploader = load[3]
+
+
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    button = telebot.types.InlineKeyboardButton(text="💾 Download", url=big_picture)
+    button2 = telebot.types.InlineKeyboardButton(text="More", callback_data='yuri')
+    try:
+        keyboard.add(button)
+        keyboard.add(button2)
+
+        bot.send_chat_action(cid, 'upload_photo')
+        bot.send_photo(cid, picture,caption='🖥Resolution: {W} x {H}\n\n👤Uploader: {uploader}\n\n#⃣Tags: {tag}'.format(H=height, W=width,
+                                                                                                          uploader=uploader,tag=tag,),
+                       reply_markup=keyboard)  # reply_markup=keyboard
+    except Exception:
+        bot.send_message(cid, "Wasn't able to proceed your request.")
+        print("An error Ocurred in /yuri:", Exception)
+        pass
 
 @bot.message_handler(commands=['cosplay', 'ecosplay','id','tag'])
 def send_nonworking_message(m):
@@ -119,34 +232,159 @@ def send_nonworking_message(m):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
-    if call.message:
-         if call.data == "generate":
+    if call.message: # procees only buttons from messages
+        if call.data == "anime":
             load = Img.anime()
-            generate = load[14]
-            width = load[8]
-            height = load[9]
-            tag = load[11]
+            picture = load[8]
+            big_picture = load[7]
+            if big_picture is None:
+                big_picture = load[8]
+            width = load[9]
+            height = load[10]
+            tag = load[1]
             uploader = load[3]
-            if load[15] is None:
-                large_file_url = load[14]
-            else:
-                large_file_url = load[15]
+
+
             keyboard = telebot.types.InlineKeyboardMarkup()
             button = telebot.types.InlineKeyboardButton(
-                text="Original {H} x {W}".format(H=height, W=width),url=large_file_url)
-            button2 = telebot.types.InlineKeyboardButton(text="More",
-                                                         callback_data='generate')
+                text="💾 Download",url=big_picture)
+            button2 = telebot.types.InlineKeyboardButton(text="More", callback_data='anime')
+
             try:
                 keyboard.add(button)
                 keyboard.add(button2)
-                bot.send_chat_action(call.message.id, 'upload_photo')
-                bot.send_photo(call.message.chat.id, generate,
-                                caption='Uploader:{uploader}\nTags: {tag}'.format(uploader=uploader,tag=tag[0, 1, 2],),
+
+                bot.send_chat_action(call.message.chat.id, 'upload_photo')
+                bot.send_photo(call.message.chat.id, picture,
+                               caption='🖥Resolution: {W} x {H}\n\n👤Uploader: {uploader}\n\n#⃣Tags: {tag}'.format(H=height, W=width,
+                                                                                                         uploader=uploader,
+                                                                                                         tag=tag,),
                                 reply_markup=keyboard)
             except Exception:
-                print("An error Ocurred:", Exception)
+                print("An error Ocurred in 'more anime':", Exception)
                 bot.send_message(call.message.id, "Wasn't able to proceed your request.")
                 pass
+        elif call.data == "ecchi":
+            load = Img.ecchi()
+            picture = load[8]
+            big_picture = load[7]
+            if big_picture is None:
+                big_picture = load[8]
+            width = load[9]
+            height = load[10]
+            tag = load[1]
+            uploader = load[3]
+
+            keyboard = telebot.types.InlineKeyboardMarkup()
+            button = telebot.types.InlineKeyboardButton(
+                text="💾 Download",url=big_picture)
+            button2 = telebot.types.InlineKeyboardButton(text="More", callback_data='ecchi')
+
+            try:
+                keyboard.add(button)
+                keyboard.add(button2)
+
+                bot.send_chat_action(call.message.chat.id, 'upload_photo')
+                bot.send_photo(call.message.chat.id, picture,
+                                caption='🖥Resolution: {W} x {H}\n\n👤Uploader: {uploader}\n\n#⃣Tags: {tag}'.format(H=height, W=width,
+                                                                                                          uploader=uploader,
+                                                                                                          tag=tag,),
+                                reply_markup=keyboard)
+            except Exception:
+                print("An error Ocurred in 'more ecchi':", Exception)
+                bot.send_message(call.message.id, "Wasn't able to proceed your request.")
+                pass
+        elif call.data == "loli":
+            load = Img.loli()
+            picture = load[8]
+            big_picture = load[7]
+            if big_picture is None:
+                big_picture = load[8]
+            width = load[9]
+            height = load[10]
+            tag = load[1]
+            uploader = load[3]
+
+            keyboard = telebot.types.InlineKeyboardMarkup()
+            button = telebot.types.InlineKeyboardButton(
+                text="💾 Download",url=big_picture)
+            button2 = telebot.types.InlineKeyboardButton(text="More", callback_data='loli')
+
+            try:
+                keyboard.add(button)
+                keyboard.add(button2)
+
+                bot.send_chat_action(call.message.chat.id, 'upload_photo')
+                bot.send_photo(call.message.chat.id, picture,
+                                caption='🖥Resolution: {W} x {H}\n\n👤Uploader: {uploader}\n\n#⃣Tags: {tag}'.format(H=height, W=width,
+                                                                                                          uploader=uploader,
+                                                                                                          tag=tag,),
+                                reply_markup=keyboard)
+            except Exception:
+                print("An error Ocurred in 'more loli':", Exception)
+                bot.send_message(call.message.id, "Wasn't able to proceed your request.")
+                pass
+        elif call.data == "hentai":
+            load = Img.hentai()
+            picture = load[8]
+            big_picture = load[7]
+            if big_picture is None:
+                big_picture = load[8]
+            width = load[9]
+            height = load[10]
+            tag = load[1]
+            uploader = load[3]
+
+            keyboard = telebot.types.InlineKeyboardMarkup()
+            button = telebot.types.InlineKeyboardButton(
+                text="💾 Download",url=big_picture)
+            button2 = telebot.types.InlineKeyboardButton(text="More", callback_data='hentai')
+
+            try:
+                keyboard.add(button)
+                keyboard.add(button2)
+
+                bot.send_chat_action(call.message.chat.id, 'upload_photo')
+                bot.send_photo(call.message.chat.id, picture,
+                                caption='🖥Resolution: {W} x {H}\n\n👤Uploader: {uploader}\n\n#⃣Tags: {tag}'.format(H=height, W=width,
+                                                                                                          uploader=uploader,
+                                                                                                          tag=tag,),
+                                reply_markup=keyboard)
+            except Exception:
+                print("An error Ocurred in 'more hentai':", Exception)
+                bot.send_message(call.message.id, "Wasn't able to proceed your request.")
+                pass
+        elif call.data == "yuri":
+            load = Img.yuri()
+            picture = load[8]
+            big_picture = load[7]
+            if big_picture is None:
+                big_picture = load[8]
+            width = load[9]
+            height = load[10]
+            tag = load[1]
+            uploader = load[3]
+
+            keyboard = telebot.types.InlineKeyboardMarkup()
+            button = telebot.types.InlineKeyboardButton(
+                text="💾 Download",url=big_picture)
+            button2 = telebot.types.InlineKeyboardButton(text="More", callback_data='yuri')
+
+            try:
+                keyboard.add(button)
+                keyboard.add(button2)
+
+                bot.send_chat_action(call.message.chat.id, 'upload_photo')
+                bot.send_photo(call.message.chat.id, picture,
+                                caption='🖥Resolution: {W} x {H}\n\n👤Uploader: {uploader}\n\n#⃣Tags: {tag}'.format(H=height, W=width,
+                                                                                                          uploader=uploader,
+                                                                                                          tag=tag,),
+                                reply_markup=keyboard)
+            except Exception:
+                print("An error Ocurred in 'more yuri':", Exception)
+                bot.send_message(call.message.id, "Wasn't able to proceed your request.")
+                pass
+
 @bot.inline_handler(lambda query: query.query == 'Hi') #useless?
 def query_text(inline_query):
     try:
@@ -167,7 +405,13 @@ def keyboard(m):
         bot.reply_to(m, en.keyboard['Test2'].format(name=m.from_user.first_name), parse_mode='markdown')
 
 def main_loop():
-    bot.polling(none_stop=True)
+    while True:
+        try:
+            bot.polling(none_stop=True)
+        except Exception:
+            pass
+        else:
+            break
     while 1:
         time.sleep(3)
 
@@ -178,4 +422,3 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print(sys.stderr, '\nExiting by user request.\n')
         sys.exit(0)
-
