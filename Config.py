7256a3d526ab80045c8aa3e67eb30d826155fe99
halkliff <werberth.lins@gmail.com>
2016-12-06 17:38:54 -0200

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,14 +4,10 @@
 
 """
 TOKEN: Access token given to you by @BotFather (https://telegram.me/BotFather
-MASTER_ID: Your actual User Unique Indentifier, looks like: 00123001023, you can look yours
-           at https://telegram.me/userinforobot
 BOT_NAME: change this to customize your bot's name. I.e.: "Cyka Bot"
 BOT_ID: change this to insert your bot's id (i.e.: Cyka_bot) NOTE: DON'T USE THE ~@~ OTHERWISE THE BOT
                                                                     WORK PROPERLY!!!
 """
-# 'Emα'
 TOKEN = 'Put your token here within the single quote'
-MASTER_ID = 'your identifier must be here'
 BOT_NAME = 'Rename me to change your bot name!'
 BOT_ID = 'Rename here with your bot ID, without the ~@~'
