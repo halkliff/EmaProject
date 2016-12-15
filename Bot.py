eff42f7a61b16cde88024662df60288c22be3d06
halkliff <werberth.lins@gmail.com>
2016-12-14 23:43:08 -0200

    Finally started working in Inline.
    Inline is not working yet, I'm trying.

--- Bot.py
+++ Bot.py
@@ -7,7 +7,6 @@ from telebot import types
 import logging
 import time
 import sys
-# import re
 from API import Img
 from lang import EN as en
 import Config
@@ -40,7 +39,7 @@ telebot.logger.setLevel(logging.DEBUG)  # Outputs debug messages to console.
 def send_welcome(m):
     from tinydb import TinyDB, Query
 
-    db = TinyDB('User_Database\db.json')
+    db = TinyDB('database/user/db.json')
 
     def new_user(from_user_id, from_user_language, from_user_nsfw_choice):
         db.insert({'id': from_user_id, 'language': from_user_language, 'nsfw': from_user_nsfw_choice})
@@ -49,9 +48,6 @@ def send_welcome(m):
         user = Query()
         a = db.search(user.id == from_user_id)
         return a
-    #markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    #button = types.KeyboardButton('1. Test')
-    #button2 = types.KeyboardButton('2. Test')
     text = m.text
     cid = m.chat.id
     match = user_search(str(m.chat.id))
@@ -69,7 +65,7 @@ def send_welcome(m):
             bot.send_message(cid, en.commandText['/start source_id'], parse_mode='markdown',)
         except Exception:
             print("An Error occurred when processing command /start source_id:", Exception)
-            
+
     if text == "/start commands":
         try:
             bot.reply_to(m, en.commandText['commands'].format(bot_id=Config.BOT_ID), parse_mode='markdown',
@@ -82,17 +78,14 @@ def send_welcome(m):
         if not match:
             try:
                 new_user(str(m.chat.id), 'English', 'Yes')
-                print(user_search(str(m.chat.id)))
                 bot.reply_to(m, "Hello *{name}*! I've saved you in my database.".format(name=m.from_user.first_name),
                              parse_mode = 'markdown')
             except Exception:
                 raise Exception
         else:
             try:
-                #markup.add(button)
-                #markup.add(button2)
                 bot.reply_to(m, en.commandText['start'].format(name = m.from_user.first_name, bot_name=Config.BOT_NAME),
-                        parse_mode='markdown',) #reply_markup=markup
+                        parse_mode='markdown',)
             except Exception:
                 print(Exception)
                 pass
@@ -102,14 +95,14 @@ def send_help(m):
     keyboard = telebot.types.InlineKeyboardMarkup(row_width=2)
     button = telebot.types.InlineKeyboardButton("❓ Usage Help", callback_data='help_use')
     button2 = telebot.types.InlineKeyboardButton("🌐 Commands", callback_data='commands')
-    #button3 = telebot.types.InlineKeyboardButton("❔ Inline Help", callback_data='inline_help')
+    button3 = telebot.types.InlineKeyboardButton("❔ Inline Help", callback_data='inline_help')
     button4 = telebot.types.InlineKeyboardButton("#⃣ Tags", callback_data='tags')
-    #button5 = telebot.types.InlineKeyboardButton("📁 Source ID", callback_data='source_id')
+    button5 = telebot.types.InlineKeyboardButton("📁 Source ID", callback_data='source_id')
 
     try:
         keyboard.add(button)
-        keyboard.row(button2, button4)
-        #keyboard.row(button3, button5)
+        keyboard.row(button2, button3)
+        keyboard.row(button4, button5)
         bot.reply_to(m, en.commandText['help'], parse_mode='markdown', reply_markup=keyboard)
     except Exception:
         print(Exception)
@@ -123,7 +116,7 @@ def send_commands(m):
     except Exception:
         print(Exception)
         pass
-"""
+
 @bot.message_handler(commands=['inline_help'])
 def send_inline_help(m):
     try:
@@ -132,7 +125,7 @@ def send_inline_help(m):
     except Exception:
         print("An Error occurred when processing command /inline_help:", Exception)
         pass
-"""
+
 @bot.message_handler(commands=['anime'])
 def send_anime(m):
     cid = m.chat.id
@@ -332,20 +325,6 @@ def send_nonworking_message(m):
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     if call.message: # procees only buttons from messages
-         #if call.data == "large_file_url":
-          #  load = Img.anime()
-           # if load[15] is None:
-            #    try:
-             #       bot.send_document(call.message.chat.id, load[14])
-              #  except Exception:
-               #     print('An Error Ocurred:', Exception)
-                #    pass
-            #else:
-             #   try:
-              #      bot.send_document(call.message.chat.id, load[15])
-               # except Exception:
-                #    print('An error Ocurred:', Exception)
-                 #   pass
         if call.data == "anime":
             try:
                 load = Img.anime()
@@ -507,7 +486,6 @@ def callback_inline(call):
                 print("An error Ocurred in 'more yuri':", Exception)
                 bot.send_message(call.message.id, "Wasn't able to proceed your request.")
                 pass
-        #elif call.data == "commands":
         elif call.data == "commands":
             try:
                 bot.send_message(call.message.chat.id, en.commandText['commands'].format(bot_id=Config.BOT_ID),
@@ -543,7 +521,7 @@ def callback_inline(call):
             except Exception:
                 print("An Error occurred when processing inline button[Usage Help]:", Exception)
 
-@bot.inline_handler(func= lambda m:True) #useless?
+@bot.inline_handler(func= lambda m:True)
 def query_text(inline_query):
     print(inline_query.from_user.first_name + ":" + inline_query.query)
 
@@ -575,65 +553,6 @@ def query_text(inline_query):
     except Exception:
         raise Exception
 
-"""
-    import requests
-    import random
-    from API.Img import Requests, Posts
-    from API import acc_setup as acc
-    l = Requests(username=acc.username, password_hash=acc.password_hash,site_name='yandere', api_name='posts_list', param='search')
-    site = '{site_url}{api_name}{login}{password}{param}{tags}{limit}'.format(site_url=l.site_url, api_name=l.api_name, login=l.username,
-                                                                              password=l.password_hash, param=l.param,
-                                                                              tags=inline_query.text, limit=Img.PARAMS['tag_limit'])
-    req = requests.get(site)
-    ret = req.json()
-    quantity = len(ret)
-    if quantity == 0:
-        r = types.InlineQueryResultArticle('1','Nobody here but chickens!', input_message_content=None)
-    else:
-        rnum = random.sample(range(0, quantity), 1)
-        num = rnum[0]
-
-        id = ret[num]["id"]
-        tags = ret[num]["tags"]
-        creator_id = ret[num]["creator_id"]
-        author = ret[num]["author"]
-        source = ret[num]["source"]
-        score = ret[num]["score"]
-        md5 = ret[num]["md5"]
-        file_url = ret[num]["file_url"]
-        sample_url = ret[num]["sample_url"]
-        width = ret[num]["width"]
-        height = ret[num]["height"]
-        sample_width = ret[num]["sample_width"]
-        sample_height = ret[num]["sample_height"]
-
-        if width and height is None:
-            width = sample_width
-            height = sample_height
-        else:
-            pass
-
-        data = Posts(id, tags, creator_id, author, source, score, md5, file_url, sample_url, width, height,
-                     sample_width, sample_height)
-
-    list = []
-
-
-
-
-
-
-    try:
-        pic = 'https://files.yande.re/image/0075af0a6017dab498a055e736b681f2/yande.re%20376607%20animal_ears%20caster_%28fate_extra%29%20fate_extra%20fate_grand_order%20fate_stay_night%20hohaku%20japanese_clothes%20tail%20thighhighs.jpg'
-        #r = types.InlineQueryResultArticle('1', 'Result1', types.InputTextMessageContent('LOL'))
-        #r2 = types.InlineQueryResultArticle('2', 'Result2', types.InputTextMessageContent('Yo'))
-        #r3 = types.InlineQueryResultArticle('3', 'Try Hard', types.InputTextMessageContent('_Tryharding_', parse_mode='markdown'))
-        r4 = types.InlineQueryResultPhoto('1', photo_url=pic, thumb_url=pic)
-        bot.answer_inline_query(inline_query.id, [r4])
-    except Exception as e:
-        print("An error Ocurred:", e)
-"""
-
 @bot.message_handler(func=lambda message: True) #Handling keyboard buttons
 def keyboard(m):
     text = m.text
@@ -642,14 +561,6 @@ def keyboard(m):
         bot.reply_to(m, en.keyboard['Test'].format(name=m.from_user.first_name), parse_mode='markdown')
     elif text == "2. Test":
         bot.reply_to(m, en.keyboard['Test2'].format(name=m.from_user.first_name), parse_mode='markdown')
-    #bot.send_chat_action(cid, 'typing')
-    #text == re.search(BotLangEN.msg['Hi'], m.text)
-
-    #if text:
-        #bot.send_message(cid, "Hey!")
-    #else:
-        #if not text:
-            #bot.send_message(cid, "Damn :/")
 
 def main_loop():
     #while True:
